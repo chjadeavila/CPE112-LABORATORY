fde80d60838a95afa358fb2685eda06fda3c4bfc
Christine Jade Avila <70692392+chjadeavila@users.noreply.github.com>
2022-02-17 13:02:50 +0800

    Add files via upload

---
+++ forloop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int numTimes, numTime;
+
+    for (numTimes = 4;
+    numTimes <=10;
+    numTime = numTimes+2);
+
+    cout <<numTimes << endl;
+
+
+}
